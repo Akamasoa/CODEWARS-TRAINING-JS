fb1ebc9bfaf9b607428d47c868b9d6057708cba6
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-08-24 10:36:54 -0300

    Create  Training JS #2: Basic data types--Number.js

---
+++ Training JS #2: Basic data types--Number.js
@@ -0,0 +1,12 @@
+/*
+In javascript, Number is one of basic data types. It can be positive: , negative: , integer:, decimal: etc..1,2,3-1,-100123,4563.1415926,-8.88
+
+Numbers can use operators such as + - * / %
+
+Task
+I've written five function , defines six global variables , every function has two local variables , please set the appropriate value for the two variables(select from v1--v6), making these function return value equal to 100. the function equal1 is completed, please refer to this example to complete the following functions.equal1,equal2,equal3,equal4,equal5v1 v2 v3 v4 v5 v6a,b
+
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+In the end, click "Submit" to submit your code pass this kata.
+*/
