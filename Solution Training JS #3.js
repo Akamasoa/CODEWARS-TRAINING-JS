5a60f467c4915343b2e577dadea1bfd647888e7d
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-08-25 23:10:03 -0300

    Update Solution Training JS #3.js

--- Solution Training JS #3.js
+++ Solution Training JS #3.js
@@ -1,4 +1,4 @@
-var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+var a1="A", a2="a", b1="B", b2="b", c1="C", c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
 
 function Dad(){
   //select some variable to combine "Dad"
