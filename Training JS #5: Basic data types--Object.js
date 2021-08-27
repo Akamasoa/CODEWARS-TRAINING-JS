017c44044578561b53ee3e8e19275b0d5013c2e7
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-08-27 11:05:27 -0300

    Create Training JS #5: Basic data types--Object.js

---
+++ Training JS #5: Basic data types--Object.js
@@ -0,0 +1,24 @@
+//In javascript, Object is one of basic data types. Define an Object can use or var obj=new Object()var obj={}
+
+//You can define the object attributes during initialization, like this:
+
+var animal={name:"dog"}
+
+//you can also set/get some properties after the object definition, like this:
+
+var animal={}
+animal.name="dog"  (or animal["name"]="dog")
+
+//Task
+
+//Give you a function , accept 1 parameter: like this:animalobj
+
+{name:"dog",legs:4,color:"white"}
+
+//and return a string like this:
+
+"This white dog has 4 legs."
+
+function animal(obj){
+  return ?
+}
