d578662fc05feca36957f7ed2d8d7c0e86bb7b1c
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-08-28 13:42:00 -0300

    Create Solution Training JS #6.js

---
+++ Solution Training JS #6.js
@@ -0,0 +1,10 @@
+/*Task
+Coding in function , function accept 1 parameters:, try to use the conditional statement if...else, 
+if value is false (val==false or it can convert to false), should return a string "false", 
+if not, return a string "true".trueOrFalsevalval
+*/
+
+function trueOrFalse(val){
+  if (val)    return 'true';             
+  else     return 'false';
+}
