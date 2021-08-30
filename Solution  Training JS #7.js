878205a091d4b27d8e178c8cea2e3b945e6c942e
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-08-30 15:54:36 -0300

    Update Solution  Training JS #7.js

--- Solution  Training JS #7.js
+++ Solution  Training JS #7.js
@@ -32,9 +32,3 @@ function saleHotdogs(n){
   return 90 * n;
   }
 }
-
-or:
-function saleHotdogs(n){
-if(n<5) return 100 * n;
-  else if(n>=5 && n<10) return 95 * n;
-  else if(n>=10) return 90 * n;
