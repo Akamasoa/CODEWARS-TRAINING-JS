9397e2d810fe418a1248f93853bbd1293055606b
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-08-29 22:33:07 -0300

    Update Solution Training JS #2.js

--- Solution Training JS #2.js
+++ Solution Training JS #2.js
@@ -1,3 +1,11 @@
+Task
+I've written five function , defines six global variables , every function has two local variables, 
+please set the appropriate value for the two variables(select from v1--v6), making these function return value equal to 100. the function equal1 is completed, 
+please refer to this example to complete the following functions.equal1,equal2,equal3,equal4,equal5v1 v2 v3 v4 v5 v6a,b
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+In the end, click "Submit" to submit your code pass this kata.
+*/
+
 var v1=50; v2=100, v3=150, v4=200, v5=2, v6=250
 
 function equal1(){
