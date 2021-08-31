9959f674501b7c0055b4fcc4d654b3be1da70b71
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-08-31 12:15:31 -0300

    Update Solution  Training JS #8.js

--- Solution  Training JS #8.js
+++ Solution  Training JS #8.js
@@ -16,6 +16,18 @@ in this month(month is always greater than 0, less than or equal to 12).
 +---------------+-------------+
 
 //little tips: Use default for most of the cases can reduce your work.
+  
+//Short solution:
+  
+  function howManydays(month){
+  switch (month){
+    case 2: return 28;
+    case 4: case 6: case 9: case 11: return 30;
+    default: return 31;
+  }
+}
+
+//Long solution:
 
   function howManydays(month){
   var days;
