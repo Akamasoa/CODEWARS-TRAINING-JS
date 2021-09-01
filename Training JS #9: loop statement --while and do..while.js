92c33ded0f37c9d451e2808868715a4a0dfc952a
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-01 11:13:44 -0300

    Update Training JS #9: loop statement --while and do..while.js

--- Training JS #9: loop statement --while and do..while.js
+++ Training JS #9: loop statement --while and do..while.js
@@ -45,4 +45,14 @@ The difference is, while judges the condition before perform code execution, do.
 Finally, it is worth mentioning: Do not forget to change the value of the variable (for conditional expressions), otherwise it will turn into an infinite loop.
 
 Ok, lesson is over. Let us do some task with while.
+
+Task
+Coding in function padIt, function accept 2 parameters:
+
+str, it's a string representing the string to pad, we need pad some "*" at leftside or rightside of str
+n, it's a number, how many times to pad the string.
+Behaviour
+You need to write a loop statement within the function that loops n times. Each time through the loop it will add one * to str, alternating on which side it is padded: the first time will add a * to the left side of str, the second time will add a * to the right side, and so on.
+
+Finally, return the padded string.
 */
