f7ed7ab162f087848d0016ed24fa08a9f5191e6a
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-07 20:54:20 -0300

    Update Training JS #10: loop statement --for.js

--- Training JS #10: loop statement --for.js
+++ Training JS #10: loop statement --for.js
@@ -1,1 +1,11 @@
+//for loop is one of the most frequently used loop statements in JS. It works like loop. In lesson 9, we wrote a while loop:while
+
+function sum1_100(){
+  var sum=0,num=1
+  while (num<=100){
+    sum+=num;
+    num++;
+  }
+  return sum;
+}
 
