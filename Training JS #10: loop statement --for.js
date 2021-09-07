ace727b58600a7a6e0bfa22bd532f473e90a7d46
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-07 20:55:50 -0300

    Update Training JS #10: loop statement --for.js

--- Training JS #10: loop statement --for.js
+++ Training JS #10: loop statement --for.js
@@ -19,3 +19,21 @@ function sum1_100(){
   return sum;
 }
 
+/*
+As you see, there are three parts in the brackets, separated by . 
+The first part is the initialization variable, this part will run before the loop starts. 
+The second part is the conditional expressions, check the expression before the start of each time loops, 
+when the value of the expression is false, loop will be terminated. 
+The third part is the statement about increasing and decreasing variables, they will run after the code block.";"
+*/
+
+//For the example above, the running order of the code is:
+
+                              <--- back to part 2
+                  -------------------------------------------
+                  |                 true                    |
+var sum=0,num=1  --->  num<=100 ?  ------> code block---> num++
+                                |  
+                           false|
+                       loop terminated
+
