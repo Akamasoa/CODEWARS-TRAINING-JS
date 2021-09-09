0aa20d657301bbdfc33eef98c7170278c9ba6db5
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-08 21:22:00 -0300

    Update Training JS #11: loop statement --break,continue.js

--- Training JS #11: loop statement --break,continue.js
+++ Training JS #11: loop statement --break,continue.js
@@ -1,1 +1,15 @@
+//We have seen in the switch statement, it can jump out the switch statement. 
+//it can also be used in the loop statement. an example:break
+
+function findFirstOddNumber(arr){
+  var result;
+  for (var i=0;i<arr.length;i++){
+    if (arr[i]%2==1){
+      result=arr[i];
+      break;
+    }
+  }
+  return result;
+}
+    
 
