15c2ff5667b2bc870c43b0d393b17bb90ad76f1a
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-10 20:50:03 -0300

    Update Training JS #13: Number object and its properties.js

--- Training JS #13: Number object and its properties.js
+++ Training JS #13: Number object and its properties.js
@@ -16,3 +16,17 @@ In this way, when the Number is called, it transforms itself into a number and t
 
 var num=Number(value);
 
+/*
+Number object has two generic object properties: constructor and prototype. all the objects in JS have these two properties. 
+they are two very important attributes. because of their importance and complexity, we will learn it in the future.
+
+In addition to the above two, the Number objects have five attributes (or called constant):
+
+MAX_VALUE: The maximum number that can be expressed in JS. Usage:Number.MAX_VALUE. Its approximate value is 1.7976931348623157e+308
+
+MIN_VALUE: The minimum number that can be expressed in JS(Close to 0, but not negative). Usage:Number.MIN_VALUE. 
+Its approximate value is 5e-324
+
+NaN: Non numeric value. Abbreviations of "Not a Number". When some arithmetic operations (such as the square root of a negative number) 
+or the result of the method are not numbers, return NaN. Usage:Number.NaN. It can be simplified and replaced with NaN.
+*/
