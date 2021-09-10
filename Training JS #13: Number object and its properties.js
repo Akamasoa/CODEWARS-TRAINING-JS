53e2a14ec6343cf6a1a0cce81797aa5c3b1925c5
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-10 20:47:54 -0300

    Update Training JS #13: Number object and its properties.js

--- Training JS #13: Number object and its properties.js
+++ Training JS #13: Number object and its properties.js
@@ -1,1 +1,10 @@
+/*
+The number is a basic data type in javascript. javascript also supports Number objects. 
+The object is the original value of the package object. When necessary, JavaScript automatically converts between 
+the original data and the objects. You can explicitly create a Number object with the constructor Number(). 
+Although it is not necessary to do so. Usage:
+*/
+
+var num=new Number(value);
+
 
