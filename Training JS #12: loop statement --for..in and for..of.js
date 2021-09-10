3dbf99f04dd7a765385b9b95002a78587dfd09b2
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-09 22:02:00 -0300

    Update Training JS #12: loop statement --for..in and for..of.js

--- Training JS #12: loop statement --for..in and for..of.js
+++ Training JS #12: loop statement --for..in and for..of.js
@@ -1,1 +1,19 @@
+//In lesson #11, we learned that for loop can traverse the array. If we want to traverse an object, 
+//we can use the variant of the for: for..in, it can traverse all the keys of the object. an example:
 
+function showObjectKeys(obj){
+  for (var key in obj){
+    console.log(key);
+  }
+}
+function showObjectValues(obj){
+  for (var key in obj){
+    console.log(obj[key]);
+  }
+}
+
+var ob={item1:"This",item2:"is",item3:"an",item4:"example"};
+console.log("keys of ob:")
+showObjectKeys(ob);
+console.log("values of ob:")
+showObjectValues(ob);
