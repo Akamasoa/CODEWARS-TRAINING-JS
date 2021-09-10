c248b64c4347a1c9697691b01bfb978e5304c0a7
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-09 22:35:58 -0300

    Update and rename Training JS #12.js to Solution Training JS #12.js

---
+++ Solution Training JS #12.js
@@ -0,0 +1,13 @@
+function giveMeFive(obj){
+  //coding here
+  array = [];
+  for (var key in obj){
+    if (key.length === 5){
+      array.push(key);
+      }
+    if (obj[key].length === 5){
+      array.push(obj[key]);
+    }
+  }
+  return array;
+}
