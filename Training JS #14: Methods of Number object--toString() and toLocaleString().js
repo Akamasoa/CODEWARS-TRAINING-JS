a90c8441e32cee991479a303338c899034d72411
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-14 23:27:48 -0300

    Update Training JS #14: Methods of Number object--toString() and toLocaleString().js

--- Training JS #14: Methods of Number object--toString() and toLocaleString().js
+++ Training JS #14: Methods of Number object--toString() and toLocaleString().js
@@ -1,1 +1,10 @@
+//This time we learn about two useful methods of Number objects: and . the basic usage of the two methods is to convert a number into a string. 
+//look at this:toString()toLocaleString()
+
+var num=111;
+var a=num.toString(), 
+    b=num.toLocaleString(), 
+    c=num+""
+console.log([a,b,c])   //put them to an array, we can see the quotes
+//output: [ '111', '111', '111' ]
 
