cd35f88b1c0ca568127ed8dc9719eb67b5486d78
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-15 23:49:47 -0300

    Update Solution  Training JS #15.js

--- Solution  Training JS #15.js
+++ Solution  Training JS #15.js
@@ -1,1 +1,27 @@
+/*
+Task
+‎Codificación en función , función aceptar 2 parámetros: y . es una matriz decimal. es un decimal. ‎howManySmallerarrnarrn
 
+‎La primera misión: dejar que todos los elementos de la matriz mantengan dos decimales (No es necesario convertir el número n). ‎
+
+‎La segunda misión: Traversal arr, contar el número del elemento que es menor que n y devolverlo.‎
+*/
+
+//for example:
+
+howManySmaller([1.234,1.235,1.228],1.24) should return 2
+howManySmaller([1.1888,1.1868,1.1838],1.19) should return 1
+howManySmaller([3.1288,3.1212,3.1205],3.1212) should return 2
+
+//Long Solution:
+
+function howManySmaller(arr,n){
+
+  Let count = 0;
+  
+  for (let i = 0; let i < arr.length; i++){
+    if (arr[i].toFixed(2) < n)
+      count++;
+  }
+  return count;
+}
