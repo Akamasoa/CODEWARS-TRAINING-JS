6d5277ef7352de9a300cff7d8fea220ed1f3d983
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-15 23:51:43 -0300

    Update Solution  Training JS #15.js

--- Solution  Training JS #15.js
+++ Solution  Training JS #15.js
@@ -25,3 +25,28 @@ function howManySmaller(arr,n){
   }
   return count;
 }
+
+//Short Solution:
+
+function howManySmaller(arr,n){
+  return arr.filter(x => +x.toFixed(2) < n).length;
+}
+
+// Function Export
+module.exports = howManySmaller
+
+//Test:
+
+const howManySmaller = require('.')
+
+test('Test 1', () => {
+  expect(howManySmaller([1.234, 1.235, 1.228], 1.24)).toBe(2)
+})
+
+test('Test 2', () => {
+  expect(howManySmaller([1.1888, 1.1868, 1.1838], 1.19)).toBe(1)
+})
+
+test('Test 3', () => {
+  expect(howManySmaller([3.1288, 3.1212, 3.1205], 3.1212)).toBe(2)
+})
