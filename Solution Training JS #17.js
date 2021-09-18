c8d603b3bb62c0b3d32c59ae0d84e55cf064d1e8
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-18 20:49:38 -0300

    Update Solution Training JS #17.js

--- Solution Training JS #17.js
+++ Solution Training JS #17.js
@@ -1,1 +1,18 @@
+/*
+Task
+Coding in function , function accept 2 parameters: and . is a string. is a char. firstToLaststrcstrc
 
+Please return the gap between the first position of and the last position of .cc
+
+If there are a lot of in the , should return a positive integer; If there is only one in , should return 0; If there is no in the , should return -1. Retrieval should not ignored the case.cstrcstrcstr
+
+for example:
+*/
+
+firstToLast("ababc","a") should return 2(2-0)
+firstToLast("ababc","c") should return 0(4-4)
+firstToLast("ababc","d") should return -1
+
+function firstToLast(str,c){
+  return str.includes(c) ? str.lastIndexOf(c) - str.indexOf(c) : -1;
+}
