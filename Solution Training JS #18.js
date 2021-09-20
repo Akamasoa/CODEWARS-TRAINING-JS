d5443c9af8eadd51de64c2e610b3de87983cdc60
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-19 21:48:30 -0300

    Update Solution Training JS #18.js

--- Solution Training JS #18.js
+++ Solution Training JS #18.js
@@ -21,3 +21,26 @@ return string
   .map(word => word.split("").join(separator))
   .join(" ");
 }
+
+// Function Export
+module.exports = splitAndMerge
+
+//Test:
+
+const splitAndMerge = require('.')
+
+test('Test 1', () => {
+  expect(splitAndMerge('My name is John', ' ')).toBe('M y n a m e i s J o h n')
+})
+
+test('Test 2', () => {
+  expect(splitAndMerge('My name is John', '-')).toBe('M-y n-a-m-e i-s J-o-h-n')
+})
+
+test('Test 3', () => {
+  expect(splitAndMerge('Hello World!', '.')).toBe('H.e.l.l.o W.o.r.l.d.!')
+})
+
+test('Test 4', () => {
+  expect(splitAndMerge('Hello World!', ',')).toBe('H,e,l,l,o W,o,r,l,d,!')
+})
