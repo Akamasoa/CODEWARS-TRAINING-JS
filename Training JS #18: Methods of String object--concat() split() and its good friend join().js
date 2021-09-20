6a2180a36c0c9b7514f2f660c0e83c3a5021c411
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-19 21:41:39 -0300

    Update Training JS #18: Methods of String object--concat() split() and its good friend join().js

--- Training JS #18: Methods of String object--concat() split() and its good friend join().js
+++ Training JS #18: Methods of String object--concat() split() and its good friend join().js
@@ -76,6 +76,22 @@ use split() and join():My name is John
 
 //Ok, lesson is over. let's us do some task.
 
+/*
+Task
+Implement a function which accepts 2 arguments: and .stringseparator
+
+The expected algorithm: split the into words by spaces, split each word into separate characters and join them back with the specified , 
+join all the resulting "words" back into a sentence with spaces.stringseparator
+
+For example:
+*/
+
+splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+
+
 function splitAndMerge(string, separator) {
 return ???
 }
