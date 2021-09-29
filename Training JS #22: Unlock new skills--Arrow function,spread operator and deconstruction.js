7b972f63f98a36186404a7cc5acac4a6329b3a8c
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-28 22:57:38 -0300

    Update Training JS #22: Unlock new skills--Arrow function,spread operator and deconstruction.js

--- Training JS #22: Unlock new skills--Arrow function,spread operator and deconstruction.js
+++ Training JS #22: Unlock new skills--Arrow function,spread operator and deconstruction.js
@@ -174,3 +174,21 @@ console.log(mul(2,1,2,3,4));
 
 //Ok, the lesson is over. Did you get it all? Let's do a task, now.
 
+/*
+Task
+Create a function shuffleIt. The function accepts two or more parameters. The first parameter arr is an array of numbers, 
+followed by an arbitrary number of numeric arrays. Each numeric array contains two numbers, which are indices for elements in arr 
+(the numbers will always be within bounds). For every such array, swap the elements. Try to use all your new skills: arrow functions, 
+the spread operator, destructuring, and rest parameters.
+
+Example:
+*/
+
+shuffleIt([1,2,3,4,5],[1,2]) should return [1,3,2,4,5]
+shuffleIt([1,2,3,4,5],[1,2],[3,4]) should return [1,3,2,5,4]
+shuffleIt([1,2,3,4,5],[1,2],[3,4],[2,3]) should return [1,3,5,2,4]
+
+function shuffleIt(arr,?){
+  //coding here...
+  
+}
