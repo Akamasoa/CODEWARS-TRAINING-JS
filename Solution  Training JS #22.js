9ecaed92c1318485a3f60f517acfb99ee81b33c2
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-28 23:08:14 -0300

    Update Solution  Training JS #22.js

--- Solution  Training JS #22.js
+++ Solution  Training JS #22.js
@@ -29,3 +29,23 @@ const shuffleIt = (array, ...swaps) => {
 
   return newArray;
 }
+
+// Function Export
+module.exports = shuffleIt
+
+/*
+Test:
+const shuffleIt = require('.')
+
+test('Test 1', () => {
+  expect(shuffleIt([1, 2, 3, 4, 5], [1, 2])).toEqual([1, 3, 2, 4, 5])
+})
+
+test('Test 2', () => {
+  expect(shuffleIt([1, 2, 3, 4, 5], [1, 2], [3, 4])).toEqual([1, 3, 2, 5, 4])
+})
+
+test('Test 3', () => {
+  expect(shuffleIt([1, 2, 3, 4, 5], [1, 2], [3, 4], [2, 3])).toEqual([1, 3, 5, 2, 4])
+})
+*/
